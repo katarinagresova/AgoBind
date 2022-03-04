81f25f017fae791b8bfb68d3305ecddce57a302c
Katarína Grešová <514001@mail.muni.cz>
2022-03-04 11:36:35 +0100

    1:1 data

--- run.py
+++ run.py
@@ -6,8 +6,8 @@ from log_utils import log_extra
 from training import get_trained_model
 
 config = {
-    "train_data" : 'data/train_set_1_10_CLASH2013_paper.tsv',
-    "test_data": 'data/evaluation_set_1_10_CLASH2013_paper.tsv',
+    "train_data" : 'data/train_set_1_1_CLASH2013_paper.tsv',
+    "test_data": 'data/evaluation_set_1_1_CLASH2013_paper.tsv',
     "eval_dset_ratio" : 0.2, #Deducted from the train set
     "batch_size" : 64,
     "gradient_accumulation_steps":4,
@@ -17,7 +17,7 @@ config = {
     "random_weights":False,
     "kmer_len" : 6,
     "stride" : 1,
-    "early_stopping_patience" : 10, 
+    "early_stopping_patience" : 5, 
     "learning_rate" : 2e-4,
     "weight_decay":0.01,
     "backbone":get_dnabert, 
