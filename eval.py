a945d6e9f7f9774a9f0a767a0e9897c54b1fe4e9
Katarína Grešová <514001@mail.muni.cz>
2022-03-04 11:50:44 +0100

    computing pr curve from probs

--- eval.py
+++ eval.py
@@ -5,19 +5,22 @@ from tqdm import tqdm
 import pandas as pd
 from sklearn.metrics import precision_recall_curve
 import matplotlib.pyplot as plt
+import numpy as np
+from scipy.special import softmax
 
-def get_test_score(predictions, labels):
+def get_test_score(probs, labels):
 
+    predictions = [1 if prob > 0.5 else 0 for prob in probs]
     score = f1_score(predictions, labels)
     print('test f1 score is', score)
     return score
 
-def compute_pr_curve(predictions, labels):
+def compute_pr_curve(probs, labels):
     print('Computing precision-recall curve')
 
     plt.figure()
 
-    precision, recall, _ = precision_recall_curve(labels, predictions)
+    precision, recall, _ = precision_recall_curve(labels, probs)
     print("PRECISIONS:", precision)
     print("RECALLS:", recall)
     plt.plot(recall, precision)
@@ -30,7 +33,7 @@ def compute_pr_curve(predictions, labels):
 
     return recall, precision
 
-def get_predictions_and_labels(test_data, encoded_samples_test, model):
+def get_probs_and_labels(test_data, encoded_samples_test, model):
     test_loader = DataLoader(
                 encoded_samples_test, 
                 sampler = SequentialSampler(encoded_samples_test), 
@@ -45,7 +48,9 @@ def get_predictions_and_labels(test_data, encoded_samples_test, model):
         outputs = model.to("cpu")(**sample)
         # outputs = model(**sample) #TODO make eval on GPU
 
-        preds = outputs.logits.argmax(-1).tolist()
+        #preds = outputs.logits.argmax(-1).tolist()
+        preds = np.array(outputs.logits.tolist())
+        preds = softmax(preds, axis=1)[:, 1]
         predictions.extend(preds)
 
     labels = pd.read_csv(test_data, sep='\t', usecols=['label']).to_numpy()
